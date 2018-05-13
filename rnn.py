8a67c72e62f0ceb1c71e1e0b8f798b16f02672ea
Pyjcsx <jameszjf95@gmail.com>
2018-05-13 11:40:54 +0800

    Create rnn.py

---
+++ rnn.py
@@ -0,0 +1,97 @@
+import torch
+from torch import nn
+from torch.autograd import Variable
+import numpy as np
+import matplotlib.pyplot as plt
+
+# torch.manual_seed(1)    # reproducible
+
+# Hyper Parameters
+TIME_STEP = 10      # rnn time step
+INPUT_SIZE = 1      # rnn input size
+LR = 0.02           # learning rate
+
+# show data
+steps = np.linspace(0, np.pi*2, 100, dtype=np.float32)
+x_np = np.sin(steps)    # float32 for converting torch FloatTensor
+y_np = np.cos(steps)
+# plt.plot(steps, y_np, 'r-', label='target (cos)')
+# plt.plot(steps, x_np, 'b-', label='input (sin)')
+# plt.legend(loc='best')
+# plt.show()
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, class_size, dropout=0.5):
+        super(RNN, self).__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.class_size = class_size
+        self.num_layers = num_layers
+
+        self.rnn = nn.LSTM(
+            input_size=self.input_size,
+            hidden_size=self.hidden_size,     # rnn hidden unit
+            num_layers=self.num_layers,       # number of rnn layer
+            batch_first=True,   # input & output will has batch size as 1s dimension. e.g. (batch, time_step, input_size)
+        )
+        self.dropout = nn.Dropout(dropout)
+        self.out = nn.Linear(self.hidden_size, self.class_size)
+
+    def forward(self, x):
+        # x (batch, time_step, input_size)
+        # h_state (n_layers, batch, hidden_size)
+        # r_out (batch, time_step, hidden_size)
+
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        r_out, _ = self.rnn(x, (h0, c0))
+
+        outs = []    # save all predictions
+        for time_step in range(r_out.size(1)):    # calculate output for each time step
+            outs.append(self.out(self.dropout((r_out[:, time_step, :]))))
+        return torch.stack(outs, dim=1)
+
+        # instead, for simplicity, you can replace above codes by follows
+        # r_out = r_out.view(-1, 32)
+        # outs = self.out(r_out)
+        # return outs, h_state
+
+# rnn = RNN()
+# print(rnn)
+#
+# optimizer = torch.optim.Adam(rnn.parameters(), lr=LR)   # optimize all cnn parameters
+# loss_func = nn.MSELoss()
+#
+#
+# plt.figure(1, figsize=(12, 5))
+# plt.ion()           # continuously plot
+#
+# for step in range(60):
+#     start, end = step * np.pi, (step+1)*np.pi   # time range
+#     # use sin predicts cos
+#     steps = np.linspace(start, end, TIME_STEP, dtype=np.float32)
+#     x_np = np.sin(steps)    # float32 for converting torch FloatTensor
+#     y_np = np.cos(steps)
+#
+#     x = Variable(torch.from_numpy(x_np[np.newaxis, :, np.newaxis]))    # shape (batch, time_step, input_size)
+#     y = Variable(torch.from_numpy(y_np[np.newaxis, :, np.newaxis]))
+#
+#     prediction = rnn(x)   # rnn output
+#     # !! next step is important !!
+#     # h_state = Variable(h_state.data)        # repack the hidden state, break the connection from last iteration
+#
+#
+#     loss = loss_func(prediction, y)         # cross entropy loss
+#     optimizer.zero_grad()                   # clear gradients for this training step
+#     loss.backward()                         # backpropagation, compute gradients
+#     optimizer.step()                        # apply gradients
+#
+#     # plotting
+#     plt.plot(steps, y_np.flatten(), 'r-')
+#     plt.plot(steps, prediction.data.numpy().flatten(), 'b-')
+#     plt.draw(); plt.pause(0.05)
+#
+# plt.ioff()
+# plt.show()
